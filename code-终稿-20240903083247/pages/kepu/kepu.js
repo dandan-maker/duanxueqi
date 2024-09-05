45ff30aada9439d3a6cc72421429d29613c183d9
zwkkxx <3220103307@zju.edu.cn>
2024-09-05 18:12:21 +0800

    Add files via upload

--- code-终稿-20240903083247/pages/kepu/kepu.js
+++ code-终稿-20240903083247/pages/kepu/kepu.js
@@ -22,4 +22,10 @@ Page({
   onClick_5() {
     wx.navigateTo({ url: '/pages/jiemian1_1/jiemian1_1' });
   },
+  handleWebViewTap: function (event) {
+    const url = event.currentTarget.dataset.url; // 获取点击时传递的外部链接
+    wx.navigateTo({
+      url: '/pages/webview/webview?url=' + encodeURIComponent(url) // 跳转到 webview 页面并传递链接
+    });
+  },
 });
